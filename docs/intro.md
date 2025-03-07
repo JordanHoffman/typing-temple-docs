330de11c976f6a01d8707570d1ad7d750cd5c49b
Jordan Hoffman <jghoffman01@gmail.com>
2025-03-07 17:05:00 -0500

    initial commit

---
+++ docs/intro.md
@@ -0,0 +1,29 @@
+---
+sidebar_position: 1
+---
+
+# About
+
+**[Typing Temple](https://www.typingtemple.com/)** is a website to practice and improve typing specifically through the bible.
+
+## History
+
+At one point I was interested in improving my typing speed. I searched for websites which allowed my to type biblical verses, but nearly all were rather outdated, slow, and somewhat glitchy. Thus **[Typing Temple](https://www.typingtemple.com/)** was born: a site which allows fine-grained control of typing preferences alongside a comfortable typing experience for biblical verses.
+
+## Main Features
+
+- Two bible versions to choose from for typing, freely available in the public domain:
+	- 1\. King James Versions - A classical bible published in 1611 which uses a much older form of English like what you would see in Shakespear. This can be both fun and challenging to read and type.
+	- 2\. World English Bible - A modern bible translation based off the American Standard Version (ASV). This allows for a more easily understandable typing experience.
+- Ability to select any chapter from the Old Testament or New Testament to type.
+- Ability to select any amount of verses within a chapter to type.
+- Options to display final statistics upon completion. These statistics inlclude:
+	- Total Characters typed
+	- Errors
+	- Accuracy
+	- Words Per Minute (wpm)
+
+## What Do I Need?
+
+**[Typing Temple](https://www.typingtemple.com/)** was primarily made to be used on a computer/laptop with a keyboard. It is possible for it to work on tablets/phones. However, many mobile devices are incompatible as their inputs are designed to allow for entire words to be placed at once rather than going letter by letter.
+
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,67 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Classic and Modern Translations',
+    Svg: require('@site/static/img/bible.svg').default,
+    description: (
+      <>
+        Typing Temple offers two public domain bible translations for typing: the classic King James Version (KJV) and the contemporary World English Bible (WEB).
+      </>
+    ),
+  },
+  {
+    title: 'Full Verse Control',
+    Svg: require('@site/static/img/typewriter.svg').default,
+    description: (
+      <>
+        Typing temple lets you decide however many verses within each chapter of the bible you would like to practice typing.
+      </>
+    ),
+  },
+  {
+    title: 'Typing Results Included',
+    Svg: require('@site/static/img/speed.svg').default,
+    description: (
+      <>
+        Choose whether or not you would like to see typing statisics such as words-per-minut (wpm) when you're finished typing.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="flex justify-center mb-6 h-40">
+        <Svg role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className="flex items-center py-8 w-full">
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
