c7018a0cdbc8058a6d55f2031809e76c9b98c1e2
Jordan Hoffman <jghoffman01@gmail.com>
2025-03-11 09:58:31 -0400

    prepare for github pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,11 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://JordanHoffman.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/typing-temple-docs/',
+	deploymentBranch: 'gh-pages',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
